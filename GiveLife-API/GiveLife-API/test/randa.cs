7b20b98b4bdebb2c43f1f03f90c835f5c7b5a481
RandaSaadZobair <Rndasaad1@gmail.com>
2021-06-03 22:51:12 +0200

    Revert "Revert "mesaage randa""
    This reverts commit 1bc676820fdcf626c0dcb9a23643c72142716ee0.

---
+++ GiveLife-API/GiveLife-API/test/randa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class randa
+{
+	public string name;
+	public int id;
+    Console.WriteLine(	);
+	public randa()
+	{
+        Console.WriteLine("my name is randa my id is 1 ^_^");
+	}
+}
