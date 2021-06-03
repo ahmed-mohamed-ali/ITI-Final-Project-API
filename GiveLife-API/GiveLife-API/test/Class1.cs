d75651c3fa71c38a9b09723d6639e23140d53ee3
mayad-mohamed <mmyada7@gmail.com>
2021-06-03 22:17:26 +0200

    mayada's commit

---
+++ GiveLife-API/GiveLife-API/test/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiveLife_API.test
+{
+    public class Class1
+    {
+        string x = "mayada";
+       
+       
+    }
+}
